4b2d4a454b7bb60e9ad9987d9015c7a126b7dcd8
jodichris17 <jodichris17@gmail.com>
2021-11-06 14:38:16 +0000

    second commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -22,7 +22,6 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-_jhr3h*f!onb*c6)2!h5)w-%itc4g$h!hdq#5d(5kn&a_c$8--'
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
